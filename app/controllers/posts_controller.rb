675abd287d88b3bcc250d17fdbaf2b4995bf46c3
Your Name <you@example.com>
2017-10-12 16:28:53 +0800

    use counter_cache to update the subscriptions_count of post automatic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,8 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user, :liked_users, { :visible_comments => :user }).page(params[:page])
-
-    post_ids = @posts.map{ |p| p.id } #订阅数
-    @subscriptions_count = Post.where( :id => post_ids ).joins(:subscriptions).group("posts.id").count
   end
-    # @subscriptions_count 这个变量是个 Hash，键是 post ID，值是订阅数，例如 {403=>59, 404=>89, 405=>10, 406=>93, 407=>10, 408=>47, 409=>90, 410=>78, 411=>79, 412=>43, 413=>58, 414=>13, 415=>61, 416=>76, 417=>97, 418=>59, 419=>41, 420=>68, 421=>44, 422=>44, 423=>85, 424=>95, 425=>12, 426=>54, 427=>78}
-    
+
   def show
     @post = Post.find(params[:id])
 
